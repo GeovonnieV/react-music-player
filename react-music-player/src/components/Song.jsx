165afe9427583c655faf24d8a04c8a9c3eb66162
geovonnie vasquez <geovonnievasquez@gmail.com>
2020-12-31 08:40:03 -0600

    song and player components made and imported

---
+++ react-music-player/src/components/Song.jsx
@@ -0,0 +1,15 @@
+import React from "react"
+
+const Song = () => {
+
+    return(
+        <div className="song-contianer">
+            <h1>Song</h1>
+            <h1>Song Name</h1>
+            <h1>Artist</h1>
+        </div>
+    )
+
+}
+
+export default Song;
