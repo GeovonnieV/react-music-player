b9c1e6f43050a78242eedaf0f819cc0e85e57277
geovonnie vasquez <geovonnievasquez@gmail.com>
2021-01-02 12:16:43 -0600

    style updated

--- react-music-player/src/components/Song.jsx
+++ react-music-player/src/components/Song.jsx
@@ -3,7 +3,7 @@ import React from "react"
 const Song = () => {
 
     return(
-        <div className="song-contianer">
+        <div className="song-container">
             <h1>Song</h1>
             <h1>Song Name</h1>
             <h1>Artist</h1>
